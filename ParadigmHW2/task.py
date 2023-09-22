00bd9d02874683c17982fa8bd5c3d28de0ee254c
галина <pgv21@yandex.ru>
2023-09-22 10:14:22 +0300

    save

---
+++ ParadigmHW2/task.py
@@ -0,0 +1,17 @@
+# Выбрана процедурная парадигма, так как программу можно разбить на мелкие подзадачи
+# и имеет простую структуру.
+
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, 10):
+            print(f"{i} * {j} = {i * j}")
+        print('')
+
+
+def main():
+    n = int(input("Введите число n: "))
+    multiplication_table(n)
+
+
+if __name__ == "__main__":
+    main()
